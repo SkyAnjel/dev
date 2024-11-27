32b6b95e979ed971776c42465a3076f7d377bde4
Vladislav Ivanov <vladislav_ivanovm@mail.ru>
2024-11-27 17:38:08 +0300

    Update App.vue

--- vue/src/components/App.vue
+++ vue/src/components/App.vue
@@ -1,6 +1,13 @@
 <template>
-  <div id="app">
-    <GridField :width="500" :height="500" :points="points" :radius="5" :gridSize="50" />
+  <div>
+    <GridField
+      :width="600"
+      :height="400"
+      :dataSets="dataSets"
+      :radius="5"
+      :gridSize="50"
+      :connectPoints="true"
+    />
   </div>
 </template>
 
@@ -8,21 +15,30 @@
 import GridField from './components/GridField.vue';
 
 export default {
-  components: {
-    GridField,
-  },
+  components: { GridField },
   data() {
     return {
-      points: [
-        { x: 50, y: 50, color: '#ff0000' },
-        { x: 100, y: 100, color: '#00ff00' },
-        { x: 150, y: 150, color: '#0000ff' },
+      dataSets: [
+        {
+          points: [
+            { x: 50, y: 50, color: '#FF0000' },
+            { x: 150, y: 150, color: '#00FF00' },
+            { x: 250, y: 200, color: '#0000FF' },
+            { x: 350, y: 300, color: '#FFA500' },
+          ],
+          lineColor: '#000',
+        },
+        {
+          points: [
+            { x: 250, y: 50, color: '#FFFF00' },
+            { x: 400, y: 200, color: '#FF00FF' },
+            { x: 450, y: 100, color: '#00FFFF' },
+            { x: 550, y: 300, color: '#FFA500' },
+          ],
+          lineColor: '#888',
+        },
       ],
     };
   },
 };
 </script>
-
-<style>
-/*стили */
-</style>
